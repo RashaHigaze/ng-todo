23835d0b147832fd18b2cd7b109cd1e64a92bf82
Your Name <you@example.com>
2021-10-19 00:23:40 +0300

    added form for new todo, fixed some material bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,6 +26,7 @@ import { TodoContainerComponent } from './todo-container/todo-container.componen
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
     MaterialModule,
   ],
   providers: [],
